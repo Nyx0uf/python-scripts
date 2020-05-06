7241c284163347c5c1e0a45645cff9e8fbe64c52
Benjamin Godard <benjgodard@gmail.com>
2020-05-06 19:52:53 +0200

    better png optim

--- png_optim.py
+++ png_optim.py
@@ -2,73 +2,57 @@
 # coding: utf-8
 
 """
-Optimize png files
+Optimize png files (pngquant + optipng)
 """
 
 from __future__ import division
 import os
 import argparse
-import multiprocessing
-import time
 import imghdr
 from pathlib import Path
-from threading import Thread, Lock
 from queue import Queue
 from shlex import quote
 from utils import common
 
-total_original_bytes = 0
-total_opt_bytes = 0
-LOCK = Lock()
-
-def optipng(path: Path) -> Path:
-    """optipng"""
-    outfile = path.with_name(str(path.name) + "-opt")
-    cmd = f'optipng -quiet -o7 -preserve -out {quote(str(outfile))} {quote(str(path))}'
-    print(f"{common.COLOR_PURPLE}{cmd}{common.COLOR_WHITE}")
-    os.system(cmd)
-    return outfile
-
 def handle_png_files(p_queue: Queue):
-    """Thread"""
+    """pngquant + optipng"""
     while p_queue.empty() is False:
         original_png_file: Path = p_queue.get()
-
-        # Optimize
         print(f"{common.COLOR_WHITE}[+] Optimizing {common.COLOR_YELLOW}{original_png_file}")
-        optimized = optipng(original_png_file)
-        if optimized.exists() is True and optimized.is_file() is True:
-            global total_original_bytes
-            global total_opt_bytes
-            LOCK.acquire()
-            total_original_bytes += original_png_file.stat().st_size
-            total_opt_bytes += optimized.stat().st_size
-            LOCK.release()
-            original_png_file.unlink() # Remove original
-            # Rename optimized version to original
-            optimized.rename(original_png_file)
-
+        # pngquant
+        pngquanted = original_png_file.with_name(f"{original_png_file.stem}.pngquant.png")
+        os.system(f"pngquant -f -o {quote(str(pngquanted))} --speed 1 --quality 75-100 --strip --skip-if-larger {quote(str(original_png_file))}")
+        # optipng
+        optimized = original_png_file.with_name(f"{original_png_file.stem}.optipng.png")
+        if pngquanted.exists():
+            os.system(f"optipng -quiet -o7 -preserve -out {quote(str(optimized))} {quote(str(pngquanted))}")
+            pngquanted.unlink()
+        else:
+            os.system(f"optipng -quiet -o7 -preserve -out {quote(str(optimized))} {quote(str(original_png_file))}")
+        original_png_file.unlink()
+        # Rename optimized version to original
+        optimized.rename(original_png_file)
         p_queue.task_done()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
-    parser.add_argument("-src", action="store", dest="src", type=Path, default=Path("."), help="Path to directory or png file")
+    parser.add_argument("-src", action="store", dest="src", type=Path, default=Path("."), help="Path to directory or single png file")
     args = parser.parse_args()
 
-    if common.which("optipng") is None:
-        common.abort("[!] optipng not found in $PATH")
+    if common.which("pngquant") is None or common.which("optipng") is None:
+        common.abort("[!] pngquant / optipng not found in $PATH")
 
     # Sanity check
     if args.src.exists() is False:
         common.abort(parser.format_help())
 
     # Get files list
-    path: Path = args.src.resolve()
-    files = common.walk_directory(path, lambda x: imghdr.what(x) == "png")
+    files = common.walk_directory(args.src.resolve(), lambda x: imghdr.what(x) == "png")
     queue = common.as_queue(files)
-    print(f"{common.COLOR_WHITE}[+] {len(files)} file{'s' if len(files) != 1 else ''} to consider")
+    total_original_bytes = sum(x.stat().st_size for x in files)
+    print(f"{common.COLOR_WHITE}[+] {len(files)} file{'s' if len(files) != 1 else ''} to optimize ({total_original_bytes / 1048576:4.2f}Mb)")
 
     # Optimize
     t = common.parallel(handle_png_files, (queue,))
-    bytes_saved = total_original_bytes - total_opt_bytes
-    print("{}[+] {} bytes saved ({:.2f}Mb) in {:.2f}s".format(common.COLOR_WHITE, bytes_saved, (bytes_saved / 1048576), t))
+    bytes_saved = total_original_bytes - sum(x.stat().st_size for x in files)
+    print(f"{common.COLOR_WHITE}[+] {bytes_saved} bytes saved ({bytes_saved / 1048576:4.2f}Mb) in {t:4.2f}s")
