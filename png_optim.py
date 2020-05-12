f82feb81d386506cb33457b9007799bb158dd3b2
Benjamin Godard <benjgodard@gmail.com>
2020-05-12 17:30:52 +0200

    make pngquant optional

--- png_optim.py
+++ png_optim.py
@@ -8,7 +8,6 @@ Optimize png files (pngquant + optipng)
 from __future__ import division
 import os
 import argparse
-import imghdr
 from pathlib import Path
 from queue import Queue
 from shlex import quote
@@ -19,7 +18,7 @@ LOGGER: logger.Logger
 def pngquant(path: Path) -> Path:
     """pngquant"""
     outfile = path.with_name(f"{path.stem}.pngquant.png")
-    cmd = f"pngquant -f -o {quote(str(outfile))} --speed 1 --quality 75-100 --strip --skip-if-larger {quote(str(path))}"
+    cmd = f"pngquant -f -o {quote(str(outfile))} --speed 1 --quality 65-80 --strip --skip-if-larger {quote(str(path))}"
     os.system(cmd)
     return outfile
 
@@ -37,25 +36,32 @@ def zopflipng(path: Path) -> Path:
     os.system(cmd)
     return outfile
 
-def handle_png_files(p_queue: Queue):
+def handle_png_files(p_queue: Queue, use_pngquant: bool):
     """pngquant + optipng"""
     while p_queue.empty() is False:
         original_png_file: Path = p_queue.get()
         LOGGER.log(f"{common.COLOR_WHITE}[+] Optimizing {common.COLOR_YELLOW}{original_png_file}")
-        # pngquant
-        pngquanted = pngquant(original_png_file)
-        # zopflipng
-        optimized = zopflipng(pngquanted if pngquanted.exists() else original_png_file)
-        if pngquanted.exists():
-            pngquanted.unlink()
-        if optimized.exists():
-            original_png_file.unlink()
-            optimized.rename(original_png_file)
+        if use_pngquant is True:
+            # zopflipng
+            optimized = zopflipng(original_png_file)
+            if optimized.exists():
+                original_png_file.unlink()
+                optimized.rename(original_png_file)
+        else:
+            pngquanted = pngquant(original_png_file)
+            # zopflipng
+            optimized = zopflipng(pngquanted if pngquanted.exists() else original_png_file)
+            if pngquanted.exists():
+                pngquanted.unlink()
+            if optimized.exists():
+                original_png_file.unlink()
+                optimized.rename(original_png_file)
         p_queue.task_done()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=Path, help="Path to directory or single PNG file")
+    parser.add_argument("-q", "--quant", dest="quant", action="store_true", help="Use pngquant (png8b)")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode")
     args = parser.parse_args()
     LOGGER = logger.Logger(args.verbose)
@@ -66,12 +72,12 @@ if __name__ == "__main__":
         common.abort(parser.format_help())
 
     # Get files list
-    files = common.walk_directory(args.input.resolve(), lambda x: imghdr.what(x) == "png")
+    files = common.walk_directory(args.input.resolve(), lambda x: x.suffix == ".png")
     queue = common.as_queue(files)
     total_original_bytes = sum(x.stat().st_size for x in files)
     LOGGER.log(f"{common.COLOR_WHITE}[+] {len(files)} file{'s' if len(files) != 1 else ''} to optimize ({total_original_bytes / 1048576:4.2f}Mb)")
 
     # Optimize
-    t = common.parallel(handle_png_files, (queue,))
+    t = common.parallel(handle_png_files, (queue, args.quant, ))
     bytes_saved = total_original_bytes - sum(x.stat().st_size for x in files)
     LOGGER.log(f"{common.COLOR_WHITE}[+] {bytes_saved} bytes saved ({bytes_saved / 1048576:4.2f}Mb) in {t:4.2f}s")
