ed9bb60f02a4f33b27d3c1daffaf7b8793700173
Benjamin Godard <benjgodard@gmail.com>
2020-05-13 13:24:24 +0200

    revamp

--- jpeg_optim.py
+++ jpeg_optim.py
@@ -8,7 +8,6 @@ Optimize jpeg files with jpegtran/mozjpeg
 from __future__ import division
 import os
 import argparse
-import imghdr
 from pathlib import Path
 from queue import Queue
 from shlex import quote
@@ -18,8 +17,15 @@ LOGGER: logger.Logger
 
 def is_420_subsampled(path: Path) -> bool:
     """Check if the jpeg file at `path` is 420"""
-    ch = common.system_call(f"identify -format %[jpeg:sampling-factor] {str(path)}").decode("utf-8").strip()
+    ch = common.system_call(f"identify -format %[jpeg:sampling-factor] {quote(str(path))}").decode("utf-8").strip()
     return '2x2,1x1,1x1' in ch
+
+def convert_to_420(path: Path) -> Path:
+    """Subsample jpeg file"""
+    outfile = path.with_name(str(path.stem) + ".420.jpg")
+    cmd = f'convert {quote(str(path))} -sampling-factor "2x2,1x1,1x1" {quote(str(outfile))}'
+    os.system(cmd)
+    return outfile
 
 def convert_to_420_and_optimize(path: Path, keep_metadata: bool) -> Path:
     """Subsample jpeg file"""
@@ -35,7 +41,14 @@ def jpegtran(path: Path, keep_metadata: bool) -> Path:
     os.system(cmd)
     return outfile
 
-def handle_jpeg_files(p_queue: Queue, keep_metadata: bool, subsample: bool):
+def guetzli(path: Path) -> Path:
+    """guetzli"""
+    outfile = path.with_name(str(path.stem) + ".guetzli.jpg")
+    cmd = f'guetzli --quality 84 {quote(str(path))} {quote(str(outfile))}'
+    os.system(cmd)
+    return outfile
+
+def handle_jpeg_files(p_queue: Queue, keep_metadata: bool, subsample: bool, use_guetzli: bool):
     """Thread"""
     while p_queue.empty() is False:
         original_jpeg_file: Path = p_queue.get()
@@ -43,17 +56,38 @@ def handle_jpeg_files(p_queue: Queue, keep_metadata: bool, subsample: bool):
         # Subsample if needed
         if subsample is True and is_420_subsampled(original_jpeg_file) is False:
             LOGGER.log(f"{common.COLOR_WHITE}[+] Subsampling {common.COLOR_YELLOW}{original_jpeg_file}")
-            subsampled = convert_to_420_and_optimize(original_jpeg_file, keep_metadata)
-            if subsampled.exists():
-                original_jpeg_file.unlink()
-                subsampled.rename(original_jpeg_file)
+            if use_guetzli is True:
+                subsampled = convert_to_420(original_jpeg_file)
+                if subsampled.exists() is True:
+                    g = guetzli(subsampled)
+                    if g.exists() is True:
+                        optimized = jpegtran(g, keep_metadata)
+                        if optimized.exists():
+                            original_jpeg_file.unlink()
+                            optimized.rename(original_jpeg_file)
+                        g.unlink()
+                    subsampled.unlink()
+            else:
+                subsampled = convert_to_420_and_optimize(original_jpeg_file, keep_metadata)
+                if subsampled.exists():
+                    original_jpeg_file.unlink()
+                    subsampled.rename(original_jpeg_file)
         else:
             # Optimize
             LOGGER.log(f"{common.COLOR_WHITE}[+] Optimizing {common.COLOR_YELLOW}{original_jpeg_file}")
-            optimized = jpegtran(original_jpeg_file, keep_metadata)
-            if optimized.exists():
-                original_jpeg_file.unlink()
-                optimized.rename(original_jpeg_file)
+            if use_guetzli is True:
+                g = guetzli(original_jpeg_file)
+                if g.exists() is True:
+                    optimized = jpegtran(g, keep_metadata)
+                    if optimized.exists():
+                        original_jpeg_file.unlink()
+                        optimized.rename(original_jpeg_file)
+                    g.unlink()
+            else:
+                optimized = jpegtran(original_jpeg_file, keep_metadata)
+                if optimized.exists():
+                    original_jpeg_file.unlink()
+                    optimized.rename(original_jpeg_file)
 
         p_queue.task_done()
 
@@ -62,22 +96,25 @@ if __name__ == "__main__":
     parser.add_argument("input", type=Path, help="Path to directory or single JPEG file")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode")
     parser.add_argument("-s", "--subsample", dest="subsample", action='store_true', help="Subsample image to 420 if needed")
+    parser.add_argument("-g", "--guetzli", dest="guetzli", action='store_true', help="Use guetzli (very slow)")
     parser.add_argument("-m", "--keep-metadata", dest="keep_metadata", action='store_true', help="Don't delete metadata")
     args = parser.parse_args()
     LOGGER = logger.Logger(args.verbose)
 
     # Sanity checks
     common.ensure_exist(["identify", "convert", "jpegtran"])
+    if args.guetzli is True:
+        common.ensure_exist(["guetzli"])
     if args.input.exists() is False:
         common.abort(parser.format_help())
 
     # Get files list
-    files = common.walk_directory(args.input.resolve(), lambda x: imghdr.what(x) == "jpeg")
+    files = common.walk_directory(args.input.resolve(), lambda x: x.suffix == ".jpeg" or x.suffix == ".jpg")
     queue = common.as_queue(files)
     total_original_bytes = sum(x.stat().st_size for x in files)
     LOGGER.log(f"{common.COLOR_WHITE}[+] {len(files)} file{'s' if len(files) != 1 else ''} to optimize ({total_original_bytes / 1048576:4.2f}Mb)")
 
     # Optimize
-    t = common.parallel(handle_jpeg_files, (queue, args.keep_metadata, args.subsample,))
+    t = common.parallel(handle_jpeg_files, (queue, args.keep_metadata, args.subsample, args.guetzli,))
     bytes_saved = total_original_bytes - sum(x.stat().st_size for x in files)
     LOGGER.log(f"{common.COLOR_WHITE}[+] {bytes_saved} bytes saved ({bytes_saved / 1048576:4.2f}Mb) in {t:4.2f}s")
--- png_optim.py
+++ png_optim.py
@@ -2,7 +2,7 @@
 # coding: utf-8
 
 """
-Optimize png files (pngquant + optipng)
+Optimize png files
 """
 
 from __future__ import division
@@ -11,73 +11,85 @@ import argparse
 from pathlib import Path
 from queue import Queue
 from shlex import quote
+from typing import List
 from utils import common, logger
 
 LOGGER: logger.Logger
+F_OPTIPNG = str("optipng")
+F_PNGQUANT = str("pngquant")
+F_ZOPFLI = str("zopfli")
 
-def pngquant(path: Path) -> Path:
-    """pngquant"""
-    outfile = path.with_name(f"{path.stem}.pngquant.png")
-    cmd = f"pngquant -f -o {quote(str(outfile))} --speed 1 --quality 65-80 --strip --skip-if-larger {quote(str(path))}"
-    os.system(cmd)
-    return outfile
+def command_for_filter(program: str, infile: Path, outfile: Path) -> str:
+    """returns the command corresponding to `filt`"""
+    if program == F_PNGQUANT:
+        return f"pngquant -f --speed 1 --quality 65-80 --strip --skip-if-larger -o {quote(str(outfile))} {quote(str(infile))}"
+    if program == F_ZOPFLI:
+        return f"zopflipng --iterations=50 --filters=01234mepb --lossy_transparent {quote(str(infile))} {quote(str(outfile))} > /dev/null"
+    if program == F_OPTIPNG:
+        return f"optipng -quiet -o7 -preserve -out {quote(str(outfile))} {quote(str(infile))}"
+    return None
 
-def optipng(path: Path) -> Path:
-    """optipng"""
-    outfile = path.with_name(f"{path.stem}.optipng.png")
-    cmd = f"optipng -quiet -o7 -preserve -out {quote(str(outfile))} {quote(str(path))}"
-    os.system(cmd)
-    return outfile
-
-def zopflipng(path: Path) -> Path:
-    """zopflipng"""
-    outfile = path.with_name(f"{path.stem}.zopflipng.png")
-    cmd = f"zopflipng --iterations=50 --filters=01234mepb --lossy_transparent {quote(str(path))} {quote(str(outfile))} > /dev/null"
-    os.system(cmd)
-    return outfile
-
-def handle_png_files(p_queue: Queue, use_pngquant: bool):
-    """pngquant + optipng"""
+def th_optimize(p_queue: Queue, programs: List[str]):
+    """Optimization thread"""
     while p_queue.empty() is False:
-        original_png_file: Path = p_queue.get()
-        LOGGER.log(f"{common.COLOR_WHITE}[+] Optimizing {common.COLOR_YELLOW}{original_png_file}")
-        if use_pngquant is True:
-            # zopflipng
-            optimized = zopflipng(original_png_file)
-            if optimized.exists():
-                original_png_file.unlink()
-                optimized.rename(original_png_file)
-        else:
-            pngquanted = pngquant(original_png_file)
-            # zopflipng
-            optimized = zopflipng(pngquanted if pngquanted.exists() else original_png_file)
-            if pngquanted.exists():
-                pngquanted.unlink()
-            if optimized.exists():
-                original_png_file.unlink()
-                optimized.rename(original_png_file)
+        infile: Path = p_queue.get()
+        last_file = infile
+        for prg in programs:
+            outfile = infile.with_name(f"{infile.stem}.{prg}.png")
+            cmd = command_for_filter(prg, last_file, outfile)
+            if cmd is None:
+                LOGGER.log(f"{common.COLOR_RED}[!] ERROR: No command for {common.COLOR_YELLOW}{prg}{common.COLOR_WHITE}")
+                continue
+            LOGGER.log(f"{common.COLOR_WHITE}[$] {common.COLOR_PURPLE}{cmd}")
+            os.system(cmd)
+            if outfile.exists() is True:
+                if outfile.stat().st_size < last_file.stat().st_size:
+                    LOGGER.log(f"{common.COLOR_WHITE}[-] {common.COLOR_BLUE}{prg} {common.COLOR_GREEN}successful{common.COLOR_WHITE} for {common.COLOR_YELLOW}{last_file}")
+                    if last_file.samefile(infile) is False:
+                        LOGGER.log(f"{common.COLOR_WHITE}[-] Removing {common.COLOR_YELLOW}{last_file}")
+                        last_file.unlink()
+                    last_file = outfile
+                else:
+                    LOGGER.log(f"{common.COLOR_WHITE}[-] {common.COLOR_BLUE}{prg} {common.COLOR_RED}unsuccessful{common.COLOR_WHITE} for {common.COLOR_YELLOW}{last_file}")
+                    outfile.unlink()
+            else:
+                LOGGER.log(f"{common.COLOR_WHITE}[-] {common.COLOR_BLUE}{prg} {common.COLOR_RED}unsuccessful{common.COLOR_WHITE} for {common.COLOR_YELLOW}{last_file}")
+        if last_file.samefile(infile) is False:
+            LOGGER.log(f"{common.COLOR_WHITE}[-] Renaming {common.COLOR_YELLOW}{last_file}{common.COLOR_WHITE} to {common.COLOR_YELLOW}{infile}")
+            infile.unlink()
+            last_file.rename(infile)
         p_queue.task_done()
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=Path, help="Path to directory or single PNG file")
-    parser.add_argument("-q", "--quant", dest="quant", action="store_true", help="Use pngquant (png8b)")
     parser.add_argument("-v", "--verbose", dest="verbose", action="store_true", help="Verbose mode")
+    parser.add_argument("-q", "--quant", dest="use_pngquant", action="store_true", help="Use pngquant (png8b)")
+    parser.add_argument("-z", "--zopfli", dest="use_zopfli", action="store_true", help="Use zopfli (very slow)")
+    parser.add_argument("-n", "--disable-optipng", dest="use_optipng", action="store_false", default=True, help="Disable optipng")
     args = parser.parse_args()
     LOGGER = logger.Logger(args.verbose)
 
+    programs: List[str] = []
+    if args.use_pngquant is True and common.which("pngquant") is not None:
+        programs.append(F_PNGQUANT)
+    if args.use_zopfli is True and common.which("zopflipng") is not None:
+        programs.append(F_ZOPFLI)
+    if args.use_optipng is True and common.which("optipng") is not None:
+        programs.append(F_OPTIPNG)
+
     # Sanity checks
-    common.ensure_exist(["pngquant", "zopflipng"])
-    if args.input.exists() is False:
-        common.abort(parser.format_help())
+    if len(programs) == 0:
+        common.abort(f"{common.COLOR_RED}[!] ERROR: No programs specified{common.COLOR_WHITE}")
 
     # Get files list
     files = common.walk_directory(args.input.resolve(), lambda x: x.suffix == ".png")
     queue = common.as_queue(files)
     total_original_bytes = sum(x.stat().st_size for x in files)
     LOGGER.log(f"{common.COLOR_WHITE}[+] {len(files)} file{'s' if len(files) != 1 else ''} to optimize ({total_original_bytes / 1048576:4.2f}Mb)")
+    LOGGER.log(f"{common.COLOR_WHITE}[+] Using {common.COLOR_BLUE}{', '.join(programs)}")
 
     # Optimize
-    t = common.parallel(handle_png_files, (queue, args.quant, ))
+    t = common.parallel(th_optimize, (queue, programs, ))
     bytes_saved = total_original_bytes - sum(x.stat().st_size for x in files)
-    LOGGER.log(f"{common.COLOR_WHITE}[+] {bytes_saved} bytes saved ({bytes_saved / 1048576:4.2f}Mb) in {t:4.2f}s")
+    LOGGER.log(f"{common.COLOR_WHITE}[+] {common.COLOR_GREEN if bytes_saved > 0 else common.COLOR_RED}{bytes_saved} bytes saved ({bytes_saved / 1048576:4.2f}Mb) in {t:4.2f}s")
