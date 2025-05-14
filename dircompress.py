0cad25b14cef05c699bd156c307294d3d2816242
Nyx0uf <benjgodard@whine.fr>
2025-05-14 13:42:52 +0200

    bin name

--- dircompress.py
+++ dircompress.py
@@ -7,6 +7,8 @@ from pathlib import Path
 from shlex import quote
 from utils import common, io
 
+BIN7Z = "7zz"
+
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("input", type=Path, help="Path to directory")
@@ -21,4 +23,4 @@ if __name__ == "__main__":
         # 7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on "asstraffic.7z" AssTraffic
         filename = quote(str(a_dir))
         print(f"file = {filename}")
-        os.system(f"7z a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on {filename}.7z {filename}")
+        os.system(f"{BIN7Z} a -t7z -m0=lzma2 -mx=9 -mfb=64 -md=32m -ms=on {filename}.7z {filename}")
